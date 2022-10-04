5962bad0142281e69622c3e8a1ac0a3dc09be5f2
yonisaka <yonisaka0@gmail.com>
2022-10-04 15:57:15 +0700

    add odd even golang

--- Code Examples/Go/oddEven.go/oddEven.go
+++ Code Examples/Go/oddEven.go/oddEven.go
@@ -7,18 +7,18 @@ func Even(number int) bool {
 }
 
 func Odd(number int) bool {
-	// Odd should return not even.
+	// Odd should return not even
 	return !Even(number)
 }
 
 func main() {
 	for i := -4; i <= 4; i++ {
-		// Test the even func.
+		// Test the even func
 		fmt.Printf("Number: %v, Even: %v", i, Even(i))
 		fmt.Println()
 	}
 
-	// Test the odd method.
+	// Test the odd method
 	if Odd(1) {
 		fmt.Println("ok")
 	}
